7167d1140238f3488f97387c435e1b1ff2cd68b6
cosmos-1885 <dmsrkql104321@gmail.com>
2023-01-18 00:36:50 +0900

    07.연산자

--- 07.연산자/예제 07-13.js
+++ 07.연산자/예제 07-13.js
@@ -1,5 +1,6 @@
 // 7.3 비교 연산자
 // 예제 07-13
+// 동등 비교. 결과를 예측하기 어렵다.
 console.log('0' == ''); // false
 console.log(0 == ''); // true
 console.log(0 == '0'); // true
--- 07.연산자/예제 07-26.js
+++ 07.연산자/예제 07-26.js
@@ -1,4 +1,5 @@
 // 7.5 논리 연산자
 // 예제 07-26
+// 암묵적 타입 변환
 console.log(!0); // true
 console.log(!'Hello'); // false
